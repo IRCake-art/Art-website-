6ad51fbd8405844304a8a23d8993465b97b74f60
Jonathan Lin <jonathanlin@Jonathans-MacBook-Pro.local>
2026-06-09 13:22:50 -0700

    Template

---
+++ app.js
@@ -0,0 +1,12 @@
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.gallery-panel');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    panels.forEach(p => p.classList.remove('active'));
+
+    tab.classList.add('active');
+    document.getElementById(tab.dataset.tab).classList.add('active');
+  });
+});
